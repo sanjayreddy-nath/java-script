68365d109e534f3f95d0327331099cf30831bf4b
sanjayreddy-nath <sanjayreddy0305@gmail.com>
2024-12-13 11:33:40 +0530

    Add files via upload

---
+++ day9.js
@@ -0,0 +1,43 @@
+let h1tag=document.getElementById("h1");
+console.log(h1tag);
+
+
+
+function changetext(){
+    h1tag.textContent="sanjay"
+    h1tag.style.backgroundColor="red"
+h1tag.style.color="white"
+h1tag.style.padding="55px"
+h1tag.style.borderRadius="1rem"
+h1tag.style.boxShadow="2px 2px 2px 2px black"
+h1tag.style.textAlign="center"
+h1tag.style.cursor="pointer"
+}
+
+let h2tag=document.getElementById("h2");
+console.log(h2tag);
+
+function changetext1(){
+    h2tag.textContent="sanjay"
+    h2tag.style.backgroundColor="red"
+h2tag.style.color="white"
+h2tag.style.padding="55px"
+h2tag.style.borderRadius="1rem"
+h2tag.style.boxShadow="2px 2px 2px 2px black"
+h2tag.style.textAlign="center"
+h2tag.style.cursor="pointer"
+}
+
+let h3tag=document.getElementById("h3");
+console.log(h3tag);
+
+function changetext2(){
+    h3tag.textContent="sanjay"
+    h3tag.style.backgroundColor="red"
+h3tag.style.color="white"
+h3tag.style.padding="55px"
+h3tag.style.borderRadius="1rem"
+h3tag.style.boxShadow="2px 2px 2px 2px black"
+h3tag.style.textAlign="center"
+h3tag.style.cursor="pointer"
+}
