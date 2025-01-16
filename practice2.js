046eee04ccdeba7af9ccbedb4620c5a03f2a6b74
sanjayreddy-nath <sanjayreddy0305@gmail.com>
2025-01-16 21:51:29 +0530

    Delete practice2.js

--- practice2.js
+++
@@ -1,8 +0,0 @@
-// const url = "https://dummyjson.com/recipes";
-const url = "https://dummyjson.com/recipes/meal-type/lunch";
-async function getData() {
-const data= await fetch(url)
-const final=await data.json()
-console.log(final)
-}
-getData()
